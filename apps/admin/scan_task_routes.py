d09795f063765d7641ff3a14f42467a5732976c8
Akhmadjon Akbarov <97084545+akhmadjonakbarov@users.noreply.github.com>
2025-09-30 11:27:59 +0500

    ref: add new scan route

--- apps/admin/scan_task_routes.py
+++ apps/admin/scan_task_routes.py
@@ -1,5 +1,3 @@
-from typing import List
-
 from fastapi import APIRouter, HTTPException
 from sqlalchemy import select
 from starlette import status
@@ -12,18 +10,16 @@ from di.user import admin_dependency
 
 router = APIRouter()
 
+from fastapi import Query
+from sqlalchemy import func
 
-from fastapi import Query
-from sqlalchemy import func
-from fastapi import Query
-from sqlalchemy import func
 
 @router.get('')
 async def get_scan_tasks(
-    db: db_dependency,
-    user: admin_dependency,
-    page: int = Query(1, ge=1, description="Page number"),
-    page_size: int = Query(10, ge=1, le=100, description="Items per page"),
+        db: db_dependency,
+        user: admin_dependency,
+        page: int = Query(1, ge=1, description="Page number"),
+        page_size: int = Query(10, ge=1, le=100, description="Items per page"),
 ):
     try:
         # total count for pagination metadata
@@ -55,7 +51,6 @@ async def get_scan_tasks(
         }
 
 
-
 @router.patch('/{task_id}/status', status_code=status.HTTP_200_OK)
 async def update_scan_task_status(
         task_id: int,
@@ -78,3 +73,36 @@ async def update_scan_task_status(
     await db.refresh(task)
 
     return {"message": "Status updated successfully", "task_id": task.id, "new_status": task.status}
+
+
+@router.delete('/delete/all', status_code=status.HTTP_204_NO_CONTENT)
+async def delete_all_scan_tasks(
+        db: db_dependency,
+        user: admin_dependency
+):
+    result = await db.execute(select(ScanTask))
+    tasks = result.scalars().all()  # Get all tasks
+
+    if not tasks:
+        raise HTTPException(status_code=404, detail="No ScanTasks found to delete")
+
+    for task in tasks:
+        await db.delete(task)
+
+    await db.commit()
+
+
+@router.delete('/delete/{task_id}', status_code=status.HTTP_204_NO_CONTENT)
+async def delete_scan_task(
+        task_id: int,
+        req: UpdateScanTaskStatusRequest,
+        db: db_dependency,
+        user: admin_dependency
+):
+    result = await db.execute(select(ScanTask).where(ScanTask.id == task_id))
+    task: ScanTask | None = result.scalar_one_or_none()
+    if not task:
+        raise HTTPException(status_code=404, detail="ScanTask not found")
+
+    await db.delete(task)
+    await db.commit()
